82887f26e1c4063a77fe40b05c49df09436c67c4
Stephan Kristyn <account@stevek.pro>
2025-02-07 16:57:22 +0100

    Updates

--- mirror_master.sh
+++ mirror_master.sh
@@ -7,13 +7,31 @@ function usage() {
   exit 1
 }
 
+
+# Checking Variables
+if [ -z "$dest_registry" ] ; then
+  echo -e "DEST_REGISTRY variable empty or missing"
+  usage;
+fi
+
+if [ -z "$source_registry" ] ; then
+  echo -e "SOURCE_REGISTRY variable empty or missing"
+  usage;
+fi
+
+
+# Displaying Configuration
+echo -e "\n Source Registry: $source_registry \n Destination Registry: $dest_registry \n"
+
+
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p"
+  copy_image($p)
 done < images-list.txt
 
 
 # Function
-function copy_image() {
-    skopeo sync --all --src docker --dest docker $source_registry/$i:$n $dest_registry/
+function copy_image($image) {
+    # skopeo copy docker://$source_registry/$image docker://$dest_registry/$image
 }
